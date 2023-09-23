9eae842a39c4d2d73cdfa23c69ff58de435e2bec
equelly <pustoselov_andrei@mail.ru>
2023-09-23 14:07:48 +0300

    add  localStorage

--- script.js
+++ script.js
@@ -9,147 +9,203 @@ let tg = window.Telegram.WebApp;
 		
  		//  user.appendChild(p);
 		
-	let store = [
-		{name:'№5',
-		zones:[
-			{number:'1',V:0,R:0,catGM:'V', delivery: false, ship: true},
-			{number:'2',V:0,R:0,catGM:'R', delivery: false, ship: false},
-			{number:'3',V:0,R:0,catGM:'R', delivery: false, ship: false},
-			{number:'4',V:0,R:0,catGM:'V', delivery: false, ship: false}
-	],
-	V:function(){
-		let sum = 0;
-			for(let i =0; i<this.zones.length; i++){
-			sum += Number(this.zones[i].V);
-			}
-			return sum;
-			},
-	R:function(){
-		let sum = 0;
-			for(let i =0; i<this.zones.length; i++){
-			sum += Number(this.zones[i].R);
-			}
-			return sum;
-			},	
-			
-	},
-		{name:'№15',
-		zones:[
-			{number:'1',V:0,R:0,catGM:'R', delivery: false, ship: false},
-			{number:'2',V:0,R:0,catGM:'R', delivery: false, ship: false},
-			{number:'3',V:0,R:0,catGM:'V', delivery: false, ship: false},
-			{number:'4',V:0,R:0,catGM:'', delivery: false, ship: false}
-	],
-	V:function(){
-		let sum = 0;
-			for(let i =0; i<this.zones.length; i++){
-			sum += Number(this.zones[i].V);
-			}
-			return sum;
-			},
-	R:function(){
-		let sum = 0;
-			for(let i =0; i<this.zones.length; i++){
-			sum += Number(this.zones[i].R);
-			}
-			return sum;
-			},	
+		
+			let store = [
+			{name:'№5',
+			zones:[
+				{number:'1',V:0,R:0,catGM:'V', delivery: false, ship: true},
+				{number:'2',V:0,R:0,catGM:'R', delivery: false, ship: false},
+				{number:'3',V:0,R:0,catGM:'R', delivery: false, ship: false},
+				{number:'4',V:0,R:0,catGM:'V', delivery: false, ship: false}
+		],
+		V:function(){
+			let sum = 0;
+				for(let i =0; i<this.zones.length; i++){
+				sum += Number(this.zones[i].V);
+				}
+				return sum;
+				},
+		R:function(){
+			let sum = 0;
+				for(let i =0; i<this.zones.length; i++){
+				sum += Number(this.zones[i].R);
+				}
+				return sum;
+				},	
+				
 		},
-		{name:'№4',
-		zones:[
-			{number:'1',V:0,R:0,catGM:'R', delivery: false, ship: false},
-			{number:'2',V:0,R:0,catGM:'R', delivery: false, ship: false},
-			{number:'3',V:0,R:0,catGM:'V', delivery: false, ship: false},
-			{number:'4',V:0,R:0,catGM:'', delivery: false, ship: false}
-	],
-	V:function(){
-		let sum = 0;
-			for(let i =0; i<this.zones.length; i++){
-			sum += Number(this.zones[i].V);
-			}
-			return sum;
+			{name:'№15',
+			zones:[
+				{number:'1',V:0,R:0,catGM:'R', delivery: false, ship: false},
+				{number:'2',V:0,R:0,catGM:'R', delivery: false, ship: false},
+				{number:'3',V:0,R:0,catGM:'V', delivery: false, ship: false},
+				{number:'4',V:0,R:0,catGM:'', delivery: false, ship: false}
+		],
+		V:function(){
+			let sum = 0;
+				for(let i =0; i<this.zones.length; i++){
+				sum += Number(this.zones[i].V);
+				}
+				return sum;
+				},
+		R:function(){
+			let sum = 0;
+				for(let i =0; i<this.zones.length; i++){
+				sum += Number(this.zones[i].R);
+				}
+				return sum;
+				},	
 			},
-	R:function(){
-		let sum = 0;
-			for(let i =0; i<this.zones.length; i++){
-			sum += Number(this.zones[i].R);
-			}
-			return sum;
-			},	
-	},
-		{name:'№9',
-		zones:[
-			{number:'1',V:0,R:0,catGM:'V', delivery: false, ship: false},
-			{number:'2',V:0,R:0,catGM:'R', delivery: false, ship: false},
-			{number:'3',V:0,R:0,catGM:'R', delivery: false, ship: false},
-			{number:'4',V:0,R:0,catGM:'V', delivery: false, ship: false}
-	],
-	V:function(){
-		let sum = 0;
-			for(let i =0; i<this.zones.length; i++){
-			sum += Number(this.zones[i].V);
-			}
-			return sum;
-			},
-			R:function(){
-				let sum = 0;
-					for(let i =0; i<this.zones.length; i++){
-					sum += Number(this.zones[i].R);
-					}
-					return sum;
-					},	
-	},
-		{name:'№10',
-		zones:[
-			{number:'1',V:0,R:0,catGM:'V', delivery: false, ship: false},
-			{number:'2',V:0,R:0,catGM:'R', delivery: false, ship: false},
-			{number:'3',V:0,R:0,catGM:'R', delivery: false, ship: false},
-			{number:'4',V:0,R:0,catGM:'', delivery: false, ship: false}
-	],
-	V:function(){
-		let sum = 0;
-			for(let i =0; i<this.zones.length; i++){
-			sum += Number(this.zones[i].V);
-			}
-			return sum;
-			},
-			R:function(){
-				let sum = 0;
-					for(let i =0; i<this.zones.length; i++){
-					sum += Number(this.zones[i].R);
-					}
-					return sum;
-					},	
+			{name:'№4',
+			zones:[
+				{number:'1',V:0,R:0,catGM:'R', delivery: false, ship: false},
+				{number:'2',V:0,R:0,catGM:'R', delivery: false, ship: false},
+				{number:'3',V:0,R:0,catGM:'V', delivery: false, ship: false},
+				{number:'4',V:0,R:0,catGM:'', delivery: false, ship: false}
+		],
+		V:function(){
+			let sum = 0;
+				for(let i =0; i<this.zones.length; i++){
+				sum += Number(this.zones[i].V);
+				}
+				return sum;
+				},
+		R:function(){
+			let sum = 0;
+				for(let i =0; i<this.zones.length; i++){
+				sum += Number(this.zones[i].R);
+				}
+				return sum;
+				},	
 		},
-		{name:'№14',
-		zones:[
-			{number:'1',V:0,R:0,catGM:'V', delivery: false, ship: false},
-			{number:'2',V:0,R:0,catGM:'V', delivery: false, ship: false},
-			{number:'3',V:0,R:0,catGM:'R', delivery: false, ship: false},
-			{number:'4',V:0,R:0,catGM:'', delivery: false, ship: false}
-	],
-	V:function(){
-		let sum = 0;
-			for(let i =0; i<this.zones.length; i++){
-			sum += Number(this.zones[i].V);
-			}
-			return sum;
+			{name:'№9',
+			zones:[
+				{number:'1',V:0,R:0,catGM:'V', delivery: false, ship: false},
+				{number:'2',V:0,R:0,catGM:'R', delivery: false, ship: false},
+				{number:'3',V:0,R:0,catGM:'R', delivery: false, ship: false},
+				{number:'4',V:0,R:0,catGM:'V', delivery: false, ship: false}
+		],
+		V:function(){
+			let sum = 0;
+				for(let i =0; i<this.zones.length; i++){
+				sum += Number(this.zones[i].V);
+				}
+				return sum;
+				},
+				R:function(){
+					let sum = 0;
+						for(let i =0; i<this.zones.length; i++){
+						sum += Number(this.zones[i].R);
+						}
+						return sum;
+						},	
+		},
+			{name:'№10',
+			zones:[
+				{number:'1',V:0,R:0,catGM:'V', delivery: false, ship: false},
+				{number:'2',V:0,R:0,catGM:'R', delivery: false, ship: false},
+				{number:'3',V:0,R:0,catGM:'R', delivery: false, ship: false},
+				{number:'4',V:0,R:0,catGM:'', delivery: false, ship: false}
+		],
+		V:function(){
+			let sum = 0;
+				for(let i =0; i<this.zones.length; i++){
+				sum += Number(this.zones[i].V);
+				}
+				return sum;
+				},
+				R:function(){
+					let sum = 0;
+						for(let i =0; i<this.zones.length; i++){
+						sum += Number(this.zones[i].R);
+						}
+						return sum;
+						},	
 			},
-			R:function(){
-				let sum = 0;
-					for(let i =0; i<this.zones.length; i++){
-					sum += Number(this.zones[i].R);
-					}
-					return sum;
-					},	
-	}
-	]	
+			{name:'№14',
+			zones:[
+				{number:'1',V:0,R:0,catGM:'V', delivery: false, ship: false},
+				{number:'2',V:0,R:0,catGM:'V', delivery: false, ship: false},
+				{number:'3',V:0,R:0,catGM:'R', delivery: false, ship: false},
+				{number:'4',V:0,R:0,catGM:'', delivery: false, ship: false}
+		],
+		V:function(){
+			let sum = 0;
+				for(let i =0; i<this.zones.length; i++){
+				sum += Number(this.zones[i].V);
+				}
+				return sum;
+				},
+				R:function(){
+					let sum = 0;
+						for(let i =0; i<this.zones.length; i++){
+						sum += Number(this.zones[i].R);
+						}
+						return sum;
+						},	
+		}
+		]	
+		
+
+		//
+	//localStorage
+	let cardzone = document.querySelector('#cardzone');
+	let titleCard = '', Card = '', listZones ='';
+
+	let keepStore= [];
+	if (localStorage.keepStore != undefined) {
+		keepStore =  JSON.parse(localStorage.keepStore);
+		let cardzone = document.querySelector('#cardzone');
+		
+		  for (let i = 0; i < keepStore.length; i++){ 
+			  
+			  Card = document.createElement('div');
+				  Card.className = "card mt-2";
+				  Card.style="min-width:20rem; margin: auto;";
+				  
+			  titleCard = document.createElement('div');
+				  titleCard.className='card-header';
+				  titleCard.innerHTML= 'перегрузка'+keepStore[i].name;
+				  titleCard.title =keepStore[i].name;
+			  Card.appendChild(titleCard);
+			  listZones =  document.createElement('ul');
+				  listZones.className ='list-group list-group-flush';
+				  listZones.title =keepStore[i].name;
+			  
+				  for (let z =0; z < keepStore[i].zones.length; z++){
+					  let li = document.createElement('li');
+					  li.className='list-group-item';
+					  li.innerHTML = 'зона №' + keepStore[i].zones[z].number  +`<input type='checkbox'class='checkmark' id = 'check${keepStore[i].name}(${z+1})' ${keepStore[i].zones[z].delivery == true ? 'checked' :""}/>  
+					  <input id='${keepStore[i].name}(${z+1})' class='cls' type='number' placeholder='${keepStore[i].zones[z].V}' value ='${keepStore[i].zones[z].V}' min = 1 max = 30>
+					  <select  class ="ml-3 p-1" style = "width: 3rem" name="categories" id = 'catGM${keepStore[i].name}(${z+1})'>
+						  
+						  <option value='${keepStore[i].zones[z].catGM}'>${keepStore[i].zones[z].catGM !==""?keepStore[i].zones[z].catGM:""}</option>
+						  <option value='R'>R</option>
+						  <option value='V'>V</option>
+						  <option value=''>нет зоны</option>
+					  </select>`;
+					  
+  
+					  listZones.appendChild(li);
+				  }
+				  
+				  Card.appendChild(listZones);
+			  
+			  
+		  
+			  cardzone.appendChild(Card);
+			  
+		  }
+		  console.log(keepStore);
+	}else{
+
+	
 	
 	
 	 
-	  let cardzone = document.querySelector('#cardzone');
-	  let titleCard = '', Card = '', listZones ='';
+	 
 		for (let i = 0; i < store.length; i++){ 
+			
 			Card = document.createElement('div');
 				Card.className = "card mt-2";
 				Card.style="min-width:20rem; margin: auto;";
@@ -187,7 +243,7 @@ let tg = window.Telegram.WebApp;
 			cardzone.appendChild(Card);
 			
 		}
-
+	}	
       
      
 	  //функция для вывода даты в формате 00.00.00
@@ -245,7 +301,7 @@ let tg = window.Telegram.WebApp;
 					 
 				//присваеваем объекту массива значение из поля  ввода
 			
-		
+		localStorage.setItem('keepStore', JSON.stringify(store));
 		console.log(store);
 		document.getElementById("demo").innerHTML= "";
 		
@@ -265,8 +321,8 @@ let tg = window.Telegram.WebApp;
 		+(store[i].zones[2].catGM !=''? store[i].zones[2].catGM +'3 = <strong>'+ store[i].zones[2].V :'')+'</strong>'
 		+(store[i].zones[3].catGM !=''? store[i].zones[3].catGM +'4 = <strong>'+ store[i].zones[3].V :'')+'</strong></p><hr>';
 			}
-	//console.log(store);
-		
+	//
+	
 	  }
 	  
 	  let Allinputs  = document.querySelectorAll("input");
