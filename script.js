409961da8314cb1b0941a66fc5dc9e430e12f77a
equelly <pustoselov_andrei@mail.ru>
2023-09-29 14:29:31 +0300

    delete message

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let tg = window.Telegram.WebApp;
  		 tg.expand();
  		 tg.MainButton.textColor = "#FFFFFF";
  		 tg.MainButton.color = "#2cab37";
-		  tg.sendMessage.text('send!');
+		 
 		//  let user = document.getElementById('user');
  		//  let p = document.createElement('p');
  		//  p.innerText = `${tg.initDataUnsafe.user.first_name}${tg.initDataUnsafe.user.last_name}`;
