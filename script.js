24ae515b53990d350151ec47f59c59ec35a8556b
equelly <pustoselov_andrei@mail.ru>
2023-09-29 11:08:25 +0300

    initData

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let tg = window.Telegram.WebApp;
  		 tg.expand();
  		 tg.MainButton.textColor = "#FFFFFF";
  		 tg.MainButton.color = "#2cab37";
+		  console.log(tg.initData);
 		//  let user = document.getElementById('user');
  		//  let p = document.createElement('p');
  		//  p.innerText = `${tg.initDataUnsafe.user.first_name}${tg.initDataUnsafe.user.last_name}`;
